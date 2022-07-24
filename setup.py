fb8dd0f27bae3d619399cd61dcdd16e054c8a278
THAKUR SSD <98035544+sachiniitdelhi2021@users.noreply.github.com>
2022-07-24 17:15:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+# with open('README.md', 'r') as fh:
+    # long_description = fh.read()
+
+setuptools.setup(
+    name='datacalculator',
+    version='0.0.1',
+    author='sachin dangi',
+    description='basic mathematic operatioins',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    keywords=['good', 'good morning'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    # python_requires='>=3.6',
+    # py_modules=['data_doctor'],
+    # package_dir={'': 'src'}
+)
